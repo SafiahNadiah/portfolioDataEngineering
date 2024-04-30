45a5c0a37893c5a7710a043c803c2b2f5bbb948f
Safiah Nadiah Abdul Latiff <safiahnadiah8891@gmail.com>
2024-05-01 01:55:13 +0800

    Create 17_Remove_Duplicates_from_a_List.py

---
+++ 17_Remove_Duplicates_from_a_List.py
@@ -0,0 +1,6 @@
+def remove_duplicates(input_list):
+    unique_list = []
+    for item in input_list:
+        if item not in unique_list:
+            unique_list.append(item)
+    return unique_list
